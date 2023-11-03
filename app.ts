7a380480b13037f8481100debda917efa8b6ea28
jk <akdirdl@gmail.com>
2023-11-04 00:40:27 +0900

    상속 형태

--- app.ts
+++ app.ts
@@ -33,6 +33,33 @@ const store: Store = {
   feeds: [],
 };
 
+class Api{
+  url: string;
+  ajax: XMLHttpRequest;
+
+  constructor(url: string){
+    this.url = url;
+    this.ajax = new XMLHttpRequest();  
+  }
+
+  protected getRequest<AjaxResponse>(): AjaxResponse{
+    this.ajax.open('GET', this.url, false);
+    this.ajax.send();
+
+    return JSON.parse(this.ajax.response);
+  }
+}
+
+class NewsFeedApi extends Api{
+  getData(): NewsFeed[] {
+    return this.getRequest<NewsFeed[]>();
+  }
+}
+class NewsDetailApi extends Api{
+  getData(): NewsDetail[] {
+    return this.getRequest<NewsDetail[]>();
+  }
+}
 function getData<AjaxResponse>(url: string): AjaxResponse {
   ajax.open('GET', url, false);
   ajax.send();
@@ -54,6 +81,7 @@ function updateView(html: string): void{
 }
 
 function newsFeed(): void {
+  const api = new NewsFeedApi(NEWS_URL);
   let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
@@ -82,7 +110,7 @@ function newsFeed(): void {
 `;
 
   if(newsFeed.length === 0){
-    newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
+    newsFeed = store.feeds = makeFeeds(api.getData());
   }
   
   for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++){
@@ -132,8 +160,10 @@ window.addEventListener('hashchange', router);
 
 function newsDetail(): void {
   const id = location.hash.substr(7);
+  const api = new NewsDetailApi(CONTENT_URL.replace('@id', id));
+  const newsContent = api.getData();
 
-  const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id))
+  
   let template = `
   <div class="bg-gray-600 min-h-screen pb-8">
   <div class="bg-white text-xl">
