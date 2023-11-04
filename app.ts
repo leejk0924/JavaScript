bd63ec7123d82d74b1fddcf968f5d59120df18f7
jk <akdirdl@gmail.com>
2023-11-04 22:48:41 +0900

    믹스인

--- app.ts
+++ app.ts
@@ -33,31 +33,35 @@ const store: Store = {
   feeds: [],
 };
 
+function applyApiMixins(targetClass: any, baseClass: any[]): void{
+  baseClass.forEach(baseClass => {
+    Object.getOwnPropertyNames(baseClass.prototype).forEach(name=>{
+      const descriptor = Object.getOwnPropertyDescriptor(baseClass.prototype, name);
+
+      if(descriptor){
+        Object.defineProperty(targetClass.prototype, name, descriptor);
+      }
+    });
+  });
+}
 class Api{
-  url: string;
-  ajax: XMLHttpRequest;
+  getRequest<AjaxResponse>(url: string): AjaxResponse{
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', url, false);
+    ajax.send();
 
-  constructor(url: string){
-    this.url = url;
-    this.ajax = new XMLHttpRequest();  
-  }
-
-  protected getRequest<AjaxResponse>(): AjaxResponse{
-    this.ajax.open('GET', this.url, false);
-    this.ajax.send();
-
-    return JSON.parse(this.ajax.response);
+    return JSON.parse(ajax.response);
   }
 }
 
-class NewsFeedApi extends Api{
+class NewsFeedApi {
   getData(): NewsFeed[] {
-    return this.getRequest<NewsFeed[]>();
+    return this.getRequest<NewsFeed[]>(NEWS_URL);
   }
 }
-class NewsDetailApi extends Api{
-  getData(): NewsDetail[] {
-    return this.getRequest<NewsDetail[]>();
+class NewsDetailApi {
+  getData(id: string): NewsDetail {
+    return this.getRequest<NewsDetail>(CONTENT_URL.replace('@id', id));
   }
 }
 function getData<AjaxResponse>(url: string): AjaxResponse {
@@ -65,6 +69,11 @@ function getData<AjaxResponse>(url: string): AjaxResponse {
   ajax.send();
   return JSON.parse(ajax.response);
 };
+
+interface NewsFeedApi extends Api{};
+interface NewsDetailApi extends Api{};
+applyApiMixins(NewsFeedApi, [Api]);
+applyApiMixins(NewsDetailApi, [Api]);
 
 function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
   for(let i = 0; i < feeds.length; i++){
@@ -81,7 +90,7 @@ function updateView(html: string): void{
 }
 
 function newsFeed(): void {
-  const api = new NewsFeedApi(NEWS_URL);
+  const api = new NewsFeedApi();
   let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
@@ -160,10 +169,8 @@ window.addEventListener('hashchange', router);
 
 function newsDetail(): void {
   const id = location.hash.substr(7);
-  const api = new NewsDetailApi(CONTENT_URL.replace('@id', id));
-  const newsContent = api.getData();
-
-  
+  const api = new NewsDetailApi();
+  const newsContent = api.getData(id);
   let template = `
   <div class="bg-gray-600 min-h-screen pb-8">
   <div class="bg-white text-xl">
