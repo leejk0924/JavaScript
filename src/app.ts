a3114a48115e3399e9f69a56e0b0ae6385d7cf5e
jk <akdirdl@gmail.com>
2023-11-08 00:49:10 +0900

    전역객체 수정

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,14 @@
 import Router from './core/router';
 import { NewsFeedView, NewsDetailView } from './page';
-import { Store } from './types';
+import { Store } from './store';
 
-const store: Store = {
-  currentPage: 1,
-  feeds: [],
-};
+const store = new Store();
 
-declare global {
-  interface Window{
-    store: Store;
-  }
-}
-
-window.store = store;
-const router: Router= new Router();
-const newsFeedView = new NewsFeedView('root');
-const newsDetailView = new NewsDetailView('root');
+const router: Router = new Router();
+const newsFeedView = new NewsFeedView('root', store);
+const newsDetailView = new NewsDetailView('root', store);
 
 router.setDefaultPage(newsFeedView);
 
-router.addRoutePath('/page/', newsFeedView);
-router.addRoutePath('/show/', newsDetailView);
- 
-router.route();
+router.addRoutePath('/page/', newsFeedView, /page\/(\d+)/);
+router.addRoutePath('/show/', newsDetailView, /show\/(\d+)/);
